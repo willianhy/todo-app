a64f649c51c1f587c9424578078c33ed09e49070
Willian Yassunaka <willianhy@gmail.com>
2017-10-29 19:09:18 -0200

    configuring initial component

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                preset: ['es2015', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         },{
