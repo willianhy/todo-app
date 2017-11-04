0436f6c7f2eee9a134d89cea7825ad22891d2797
Willian Yassunaka <willianhy@gmail.com>
2017-11-04 15:09:13 -0200

    search & remove

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash-o'
+                        onClick={() => props.handleRemove(todo)}>
+                    </IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thread>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thread>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
